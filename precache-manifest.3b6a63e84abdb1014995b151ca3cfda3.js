e759096056b2ec8e48ea2f78a1bf1f4aa0df504a
Maria <khangi123go@gmail.com>
2019-11-24 13:45:52 +0500

    Updates

---
+++ precache-manifest.3b6a63e84abdb1014995b151ca3cfda3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20f6e1f19891492ab60a3fca827c6de",
+    "url": "/news-world/index.html"
+  },
+  {
+    "revision": "34658d339e7bbce1ae46",
+    "url": "/news-world/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b9d3e068de98a1a3f36e",
+    "url": "/news-world/static/css/main.7b96e0fe.chunk.css"
+  },
+  {
+    "revision": "34658d339e7bbce1ae46",
+    "url": "/news-world/static/js/2.81dd6a38.chunk.js"
+  },
+  {
+    "revision": "b9d3e068de98a1a3f36e",
+    "url": "/news-world/static/js/main.5e115d20.chunk.js"
+  },
+  {
+    "revision": "786561bb7c79714cb535",
+    "url": "/news-world/static/js/runtime-main.6d7eb2ce.js"
+  }
+]);
